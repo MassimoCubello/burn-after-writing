d2581b93bbdc569cbafdf85cf33005e588d57101
Massimo Cubello <massimocubello41@gmail.com>
2026-03-12 18:53:33 -0400

    Fix Railway port and deployment issues

--- server.js
+++ server.js
@@ -106,6 +106,8 @@ app.post("/api/respond", async (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log("Burn After Writing running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Burn After Writing running on port ${PORT}`);
 });
